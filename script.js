4d63661bf5c56d0ab76c5cad885c34e77a58dd02
Blargh14 <59298248+Blargh14@users.noreply.github.com>
2026-01-27 08:38:27 -0500

    Show clickthrough capability

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-test = document.querySelector("#cursor");
+spotlight = document.querySelector("#cursor");
 
-document.addEventListener("mousemove", (e) => {
-    test.style.left = (e.clientX - test.width / 2) + "px";
-    test.style.top = (e.clientY - test.height / 2) + "px";
+document.addEventListener("mousemove", e => {
+    spotlight.style.left = (e.clientX - spotlight.width / 2) + "px";
+    spotlight.style.top = (e.clientY - spotlight.height / 2) + "px";
+});
+
+test = document.querySelector(".test");
+
+test.addEventListener("mousedown", e => {
+    console.log("hello");
 });
