1077f11dcd70ab197eb49308fbb0c27e9407f4ee
Blargh14 <59298248+Blargh14@users.noreply.github.com>
2026-01-27 09:14:57 -0500

    Add the spotlight with relative size

--- script.js
+++ script.js
@@ -4,9 +4,3 @@ document.addEventListener("mousemove", e => {
     spotlight.style.left = (e.clientX - spotlight.width / 2) + "px";
     spotlight.style.top = (e.clientY - spotlight.height / 2) + "px";
 });
-
-test = document.querySelector(".test");
-
-test.addEventListener("mousedown", e => {
-    console.log("hello");
-});
