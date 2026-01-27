0a2ff56bfb105bb37830d2c527e8b36e1bf609af
Blargh14 <59298248+Blargh14@users.noreply.github.com>
2026-01-27 13:39:55 -0500

    Early event test

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-spotlight = document.querySelector("#cursor");
+const spotlight = document.querySelector("#cursor");
+const background = document.querySelector("#background");
 
 document.addEventListener("mousemove", e => {
     spotlight.style.left = (e.clientX - spotlight.width / 2) + "px";
     spotlight.style.top = (e.clientY - spotlight.height / 2) + "px";
 });
+
+let active = false;
+
+for (let i = 0; i < 10; i++) {
+    addSpotlightEvent();
+}
+
+function addSpotlightEvent() {
+    let reveal = document.createElement("img")
+    reveal.src = "redsquare.jpg";
+    reveal.style.position = "absolute";
+    reveal.style.left = Math.random() * window.innerWidth + "px";
+    reveal.style.top = Math.random() * window.innerWidth + "px";
+    background.appendChild(reveal)
+}
