2ff34ab2850bd197d8697c29611fbad9e4e5e54d
Blargh14 <59298248+Blargh14@users.noreply.github.com>
2026-01-28 10:59:04 -0500

    Add directional indicator variable
    If mousemove detects a change of around 3 pixels from the last known
    poll, it'll fire the check.

--- script.js
+++ script.js
@@ -40,21 +40,56 @@ deleteThis.addEventListener("change", e => {
 });
 
 let active = false;
+let lastX = 0;
+let lastY = 0;
+
 
 document.addEventListener("mousemove", e => {
+    // When going too slow mousemove will fire based on touched pixels messing with the math
+    const tooSlow = [-2,-1,0,1,2]
+
+    if (tooSlow.includes(e.clientY - lastY) && tooSlow.includes(e.clientX - lastX)) {
+        return;
+    }
+
+    let pitch = e.clientY < lastY ? "upper" : "lower";
+    let yaw = e.clientX < lastX ? "left" : "right";
+
+    let direction = pitch + yaw;
+
+    lastX = e.clientX;
+    lastY = e.clientY;
+
+    switch (direction) {
+        case "upperleft":
+            console.log(direction);
+            break;
+        case "upperright":
+            console.log(direction);
+            break;
+        case "lowerleft":
+            console.log(direction);
+            break;
+        case "lowerright":
+            console.log(direction);
+    }
+
     spotlight.style.left = (e.clientX - spotlight.width / 2) + "px";
     spotlight.style.top = (e.clientY - spotlight.height / 2) + "px";
 
-    spawn = Math.floor(Math.random() * 100) === 99; // mousemove works faster in dev tools, something to keep in mind.
+    if (!active) {
+        spawn = Math.floor(Math.random() * 10) === 9; // mousemove works faster in dev tools, something to keep in mind.
 
-    if (spawn) {
-        active = true;
-        addSpotlightEvent();
+        if (spawn) {
+            active = true;
+            addSpotlightEvent();
+        }
     }
 });
 
 function addSpotlightEvent() {
     if (!eventLinks) {
+        active = false;
         return;
     }
 
@@ -62,11 +97,12 @@ function addSpotlightEvent() {
     const reveal = document.createElement("img");
     reveal.src = eventLinks[pick];
     reveal.style.position = "absolute";
-    reveal.style.left = Math.random() * window.innerWidth + "px";
-    reveal.style.top = Math.random() * window.innerWidth + "px";
+    reveal.style.left = Math.random() * (window.innerWidth - 100) + "px";
+    reveal.style.top = Math.random() * (window.innerHeight - 100) + "px";
     background.appendChild(reveal)
 
     reveal.addEventListener("mouseout", (e) => {
         e.currentTarget.remove();
+        active = false;
     });
 }
